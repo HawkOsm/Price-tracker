6df3c813dfbe8192cbf9f81971e0b50d925430c1
HawkOsm <osmansahinguler@gmail.com>
2025-11-02 18:53:48 +0300

    The system is working.

---
+++ run_on_start.bat
@@ -0,0 +1,10 @@
+@echo off
+#Change the location of file for your computer
+python "full path to on_open.py"
+
+'''
+Press Win+R and write shell:startup then place this folder to that folder.
+
+  What will this do is it will run the on_open.py every time the computer
+re opens and check weather there is a change in prices of products
+'''
